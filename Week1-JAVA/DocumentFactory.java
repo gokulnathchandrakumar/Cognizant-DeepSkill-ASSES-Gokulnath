298c3169fda83ec99112db35ebcacb2b72758f61
gokulnathchandrakumar <gokulnathchandrakumar@gmail.com>
2026-06-22 12:25:27 +0530

    Removed old Week1-JAVA and Week1_Queries folders

--- Week1-JAVA/DocumentFactory.java
+++
@@ -1,9 +0,0 @@
-public abstract class DocumentFactory {
-    public abstract Document createDocument();
-
-    public Document getDocument() {
-        Document doc = createDocument();
-        System.out.println("Factory produced: " + doc.getClass().getSimpleName());
-        return doc;
-    }
-}
